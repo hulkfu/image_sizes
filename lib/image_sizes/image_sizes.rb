93c786ee99ab384e9c528b2a10da0eab73cd87cd
Fu Hao <fudhao@gmail.com>
2015-10-30 00:06:40 +0800

    add more sizes.

--- lib/image_sizes/image_sizes.rb
+++ lib/image_sizes/image_sizes.rb
@@ -6,7 +6,7 @@ module ImageSizes
     puts "Get image at: " + image.path
     image.format "png"
 
-    ios_icon_sizes = [29, 40, 50, 57, 58, 60, 72, 76, 100, 114, 120, 152]
+    ios_icon_sizes = [29, 40, 50, 57, 58, 60, 72, 76, 80, 87, 100, 114, 120, 144, 180, 152, 512, 1024]
     apple_watch_icon_sizes = [196, 172, 88, 87, 80, 58, 55, 48]
 
     all_sizes = ios_icon_sizes + apple_watch_icon_sizes
